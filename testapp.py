30c25bcfb993527db55a6c43c8a5601447d4f4b9
Devs Team <129137053+MindFluent@users.noreply.github.com>
2023-03-28 11:52:08 +0800

    Update testapp.py

--- testapp.py
+++ testapp.py
@@ -9,4 +9,8 @@ df = pd.DataFrame(
     ]
 )
 
-st.dataframe(df, use_container_width=True)
+df = load_data()
+edited_df = st.experimental_data_editor(df) # 👈 An editable dataframe
+
+favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
